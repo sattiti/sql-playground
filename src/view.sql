97ded307b06e8ff9c18b79a20fe753a1e1805a69
sattiti <sattiti@users.noreply.github.com>
2020-02-26 08:21:50 +0900

    update

--- src/view.sql
+++ src/view.sql
@@ -2,6 +2,7 @@
 -- テーブルに似たものだが、仮想テーブルなのでデータは SELECT 文に保存され実態がない。
 -- 複数のテーブルに跨ることができ、よく使う SELECT 文を VIEW に保存しておくと便利。
 -- ORDER BY, UPDATE することができない。
+-- view の上に view を重ねることはパフォーマンスの低下に繋がるため、多重生成を避けるべき。
 
 -- CREATE VIEW
 CREATE VIEW {VIEW_NAME}
