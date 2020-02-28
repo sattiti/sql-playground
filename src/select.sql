ca8ac775df73604a2ca22274f47f16ff96da40cd
sattiti <sattiti@users.noreply.github.com>
2020-02-28 12:38:24 +0900

    update

--- src/select.sql
+++ src/select.sql
@@ -1,9 +1,11 @@
 -- SELECT
-/*
-https://www.w3schools.com/sql/
-*/
+-- https://www.w3schools.com/sql/
 
 -- DISTINCT (重複除外)
-SELECT DISTINCT COL_NAME1, COL_NAME2, ...
-  FROM TB_NAME
+SELECT DISTINCT
+  COL_NAME1, COL_NAME2, ...
+  FROM
+    TB1 AS ALIAS1,
+    TB2 AS ALIAS2
+  WHERE ...
 ;
