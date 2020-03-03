4a1b635bf58a0e8022681103feeb960be2092afc
sattiti <sattiti@users.noreply.github.com>
2020-03-04 08:12:59 +0900

    update

--- src/window_function.sql
+++ src/window_function.sql
@@ -1,27 +1,19 @@
--- ウィンドウ関数
--- OLAP (OnLine Analytical Processing)
--- データベースを使ってリアルタイムでにデータ分析をする。
+-- window 関数 {{{
+-- window 関数の実態はソート。
+-- window 関数は OLAP (OnLine Analytical Processing)関数とも呼ばれてる。
+-- データベースを使ってリアルタイムでデータ分析をする。
 -- レコードを集約せず、入力と同じ数のレコードを返す。
--- PARTITION BY 句による、レコード集合の分割。
--- ORDER BY によるレコードの順序付け。
--- フレームによるカレントレコードを中心したサブセットの定義。
+-- }}}
 
+-- window 関数の構成 {{{
+-- PARTITION BY
+-- レコード集合を分割する。
 
--- window 関数として使える代表的な関数。
--- 1. 集約関数
--- SUM()
--- AVG()
--- COUNT()
--- MAX()
--- MIN()
+-- ORDER BY
+-- レコードの順序付け。
 
--- 2. window 専用関数
--- RANK()
--- DENSE_RANK()
--- ROW_NUMBER()
-
-
--- FRAME 句(開始点と終了点の指定)
+-- FRAME
+-- カーソル機能。カレントレコードを中心したレコード集合の範囲を定義する。(開始点と終了点の指定)
 -- ROWS                移動単位を行で設定する。
 -- RANGE               移動単位を列の値で設定する。基準となる列は ORDER BY 句で指定された列。
 -- UNBOUNDED PRECEDING 先頭の行(終了点では使用不可)
@@ -33,7 +25,22 @@
 -- example:
 -- ROWS BETWEEN 2 FOLLOWING AND 2 FOLLOWING 2行先。
 -- ROWS BETWEEN 1 PRECEDING AND 1 PRECEDING 1行前。
+--}}}
 
+
+-- window 関数として使える代表的な関数 {{{
+-- 1. 集約関数
+-- SUM()
+-- AVG()
+-- COUNT()
+-- MAX()
+-- MIN()
+
+-- 2. window 専用関数
+-- RANK()
+-- DENSE_RANK()
+-- ROW_NUMBER()
+-- }}}
 
 -- 無名 WINDOW FUNCTION{{{
 SELECT
